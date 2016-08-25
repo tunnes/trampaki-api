c09460a4da8f057adce8d962b35d1bc22ca79141
Ayrton Felipe <ayrton.tunes@outlook.com>
2016-08-25 16:13:58 +0000

    Dinamizando o carregamento markadores JavaScript/PHP.

--- js/mapEngine.js
+++ js/mapEngine.js
@@ -24,7 +24,7 @@ function mapEngine(){
             }
         };
         
-    //  Estilizando o mapa
+    //  Estilizando o mapa --------------------------------------------------------------------------------------------------
         function estiloDoMapa(){
             var styles = [
                 {stylers: [{hue: "#FF8C1F"}, {saturation: 60}, { lightness: -20 }, { gamma: 1.51 }]},
@@ -37,29 +37,7 @@ function mapEngine(){
             mapa.setMapTypeId('map_style');
         }
         
-    /*  Desenhando o circulo no centro da tela:
-        function circuloCentral(){
-            var cityCircle = new google.maps.Circle({
-              strokeColor: '#FF3110',
-              strokeOpacity: 0.35,
-              strokeWeight: 1,
-              fillColor: '#FF3110',
-              fillOpacity: 0.35,
-              map: mapa,
-              center: {lat: -23.96425614, lng: -46.38520819},
-              radius:  5000
-            });
-            var direction = 1;
-            var rMin = 4000, rMax = 5000;
-            setInterval(function() {
-                var radius = cityCircle.getRadius();
-                if ((radius > rMax) || (radius < rMin)) {
-                    direction *= -1;
-                }
-                cityCircle.setRadius(radius + direction * 50);
-                }, 50);
-        }
-    */
+    //  Alcance do Usuario --------------------------------------------------------------------------------------------------        
         function circuloCentral(){
             var c1 = new google.maps.Circle({
               strokeColor: '#FF3110',
@@ -94,63 +72,50 @@ function mapEngine(){
         mapa = new google.maps.Map(document.getElementById("mapa"), configuracoes);
         estiloDoMapa();
         circuloCentral();
-        
-        
     }
     
-//  GERAR MARCADORES --------------------------------------------------------------------------------------------------------    
-    function carregarMarcadores(){
-        
-    //  O objeto 'configuracoesMarcador01' contem as informações de localização, 
-    //  titulo e mapa onde seram exibidos. Caras, se virem com isso que ta meio 
-    //  na cara o que cada atributo faz.. --'
-        var configuracoesMarcador01 = {
-            position: new google.maps.LatLng(-23.98154798, -46.45915708),
-            title: "M-01 Casa",
-            map: mapa,
-            icon: "../img/blackHoleSun.png",
-            animation: google.maps.Animation.DROP
-        };
-        
-    //  Na instancia 'marcador01' passei como parâmetro o objeto cirado 'configuraçõesMarcador01'.
-        var marcador01 = new google.maps.Marker(configuracoesMarcador01);
-        marcador01.addListener('click', function() {
-            document.getElementById('notification').style.display = "block";
-        });
-        
-        var configuracoesMarcador02 = {
-            position: new google.maps.LatLng(-23.97915736, -46.31190878),
-            title: "M02 - Fatec",
-            map: mapa,
-            icon: "../img/blackHoleSun.png",
-            animation: google.maps.Animation.BOUNCE
-        };
-        var marcador02 = new google.maps.Marker(configuracoesMarcador02);
-        
-        function carregarInfoWindown(){
-              var contentString =
-                   '<h2>Minha Casa</h2>'
-                  +'<p>O marcador lindo do Ayrton *-*.</p>'
-                  +'<br>'
-                  +'<img src="../img/eu.jpg" height="100" width="100">'
-                  +'<br>'
-                  +'<a href="https://github.com/tunnes" target="_blank">GitHub</a>';
-              var infowindow = new google.maps.InfoWindow({
-                  content: contentString,
-                  maxWidth: 200
-              });
-            /*   google.maps.event.addListener(marcador01, 'click', function() {
-                infowindow.open(mapa,marcador01);
-              });
-            */
+//  CARREGAR MARCADORES -----------------------------------------------------------------------------------------------------    
+    function marcadores(){
+        function loadJSON(path, success, error){
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function(){
+                if (xhr.readyState === XMLHttpRequest.DONE){
+                    if (xhr.status === 200) {
+                        if (success)
+                            success(JSON.parse(xhr.responseText));
+                    } else {
+                        if (error)
+                            error(xhr);
+                    }
+                }
+            };
+            xhr.open("GET", path, true);
+            xhr.send();
         }
-        carregarInfoWindown();
-        
+        loadJSON('js/responseMark.json', function(data){ carregarMarcadores(data);}, function(xhr){console.error(xhr);});
+        function carregarMarcadores(data){
+            var arrayResponse = [].slice.call(data)
+                arrayResponse.forEach(function(objeto){
+                    var marcador = new google.maps.Marker({
+                        position: new google.maps.LatLng(objeto.latitude, objeto.longitude),
+                        title: objeto.titulo,
+                        icon: "../img/blackHoleSun.png",
+                        map: mapa,
+                        animation: google.maps.Animation.DROP,
+                        imagem: objeto.imagem,
+                        descricaoSimples: objeto.descricaoSimples,
+                        estrelas: objeto.estrelas
+                    });
+                    marcador.addListener('click', function() {
+                        document.getElementById('notification').style.display = "block";
+                    });
+                });
+        }
     }
 
 //  CHAMADA DE FUNCOES ------------------------------------------------------------------------------------------------------
     inicializar();
-    carregarMarcadores();
+    marcadores();
 }
 
 
