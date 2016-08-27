5596fc8109b3971a7540cd719f9d290dca281367
Ayrton Felipe <ayrton.tunes@outlook.com>
2016-08-27 17:03:25 +0000

    Alteração de comentarios no index.php.

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <hr>
 <?php
-    # https://www.youtube.com/watch?v=_TjHKWdOF94 CRUD
-    
-    # Entendo melhor o PDO (Curso completo)
-    # https://www.youtube.com/watch?v=etRFu_eJ3vU&list=PLbXvLovKLUIkE78UNFE8UpfOtoDlrpQec
     
     include ('formLogin.php');
     include ('router.php');
     include ('primeira.php');
     include ('home.php');
-    
     
     $roteador =  new Router();
     
